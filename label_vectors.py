2ca3618cd609bcbb6f1e2109261ba894930b4beb
Maya Chari <mayachari@MacBook-Air-233.fios-router.home>
2023-07-20 15:15:32 -0400

    removed path input

--- label_vectors.py
+++ label_vectors.py
@@ -27,66 +27,19 @@ def create_df(csv_file):
         # iterate through the columns of each row
         # for column, value in row.items():
         for column_idx, (column, value) in enumerate(row.items()):
+            # skip the empty/label columns
             if (column_idx < 2):
                 continue;
+            # go to the next row if the cell has no value
             if pd.isna(value):
                 break;
             if (value != ""):
+                # each row in the new data frame will have label-tag-embedding
                 new_entry = [label, value, None]
                 label_tag_df = pd.concat([label_tag_df, pd.DataFrame([new_entry], columns=label_tag_df.columns)], ignore_index=True)
 
     return label_tag_df
 
-    
-    print('end of create_df function')
-
-
-# function that takes in a path to a csv file and outputs a dictionary where the 
-# keys are labels and the values are a list of tags associated with that label
-
-def read_csv_df(csv_file):
-    # initialize dictionary
-    label_tag_dict = {}
-
-    # open the csv
-    with open(csv_file, 'r') as file:
-        reader = csv.reader(file)
-        header = next(reader)
-
-        # the labels are in the 2nd column of the file
-        label_column_index = 1
-        
-        # iterate through each row in the csv file
-        for row in reader:
-            label = row[label_column_index]
-
-            # if the current row has no label, skip this row and move on to the next row
-            if (label == ""):
-                continue
-
-            tags = list()
-
-            # iterate through the length of the row (the number of ciolumns)
-            for i in range(label_column_index + 1, len(row)):
-                # if the cell in the csv file has content, add the tag to the tags list
-                if (row[i] != ""):
-                    tags.append(row[i])
-                    
-            # add this row of the csv to the output dictionary
-            label_tag_dict[label] = tags
-
-        # convert the dictionary to a dataframe
-        label_tag_df = pd.DataFrame(columns=['label', 'tag', 'embedding'])
-
-        # iterate through the dictionary to populate the dataframe
-        for key, value in label_tag_dict.items():
-            # iterate through each tag in the dictionary
-            for s in value:
-                # each row in the dataframe is label-tag-embedding
-                new_entry = [key, s, None]
-                label_tag_df = pd.concat([label_tag_df, pd.DataFrame([new_entry], columns=label_tag_df.columns)], ignore_index=True)
-
-    return label_tag_df
 
 def label_tag_embeddings(label_tag_df):
     # create an instance of the EmbeddingPredictionClient class
@@ -111,34 +64,20 @@ def label_tag_embeddings(label_tag_df):
     return label_tag_df
 
 
-
 # streamlit code
 st.title("Generating Embeddings From csv File")
 
-path_to_csv = st.text_input("Enter the path (from root) to the csv that you are uploading")
 file = st.file_uploader("Choose csv file from your computer: ")
 
 dataframe_button_clicked = st.button("Generate Dataframe")
 
-
 if dataframe_button_clicked:
-    # if a path is NOT inputted
-    if (len(path_to_csv) == 0):
-        label_tag_df = create_df(file)
-        st.write(label_tag_df)
 
-        df_with_embeddings = label_tag_embeddings(label_tag_df)
-        st.write(df_with_embeddings)
-    # if a path IS inputted
-    else:
-        file_path = file.name
-        final_path = path_to_csv + file_path
-        st.write(final_path)
-        label_tag_df = read_csv_df(final_path)
-        st.write(label_tag_df)
+    label_tag_df = create_df(file)
+    st.write(label_tag_df)
 
-        df_with_embeddings = label_tag_embeddings(label_tag_df)
-        st.write(df_with_embeddings)
+    df_with_embeddings = label_tag_embeddings(label_tag_df)
+    st.write(df_with_embeddings)
 
 
 # path_to_csv: /Users/mayachari/Downloads/
